c786423ce101ebd12c42f6959e1b2c019186c85f
GTK-kala <gtkkala1110@gmail.com>
2025-06-23 20:03:12 +0300

    Twentieth commit

--- React-WebApp/src/Context/ShopContext.jsx
+++ React-WebApp/src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import {products} from '../frontend_assets/assets';
 
 export const ShopContext = createContext();
@@ -8,10 +8,35 @@ export const ShopContext = createContext();
        const currency = '$';
        const delvery_fee = 10;
         const [search , setSearch] = useState();
-        const [showSearch , setShowSearch] = useState(false)
+        const [showSearch , setShowSearch] = useState(false);
+        const [cartItems , setCartItems] = useState({});
+
+        const addToCart = async (itemId , size) =>{
+            let cartDatas = structuredClone(cartItems);
+
+            if(cartDatas[itemId]){
+              if(cartDatas[itemId][size]){
+                cartDatas[itemId][size] += 1;
+              }
+              else{
+                cartDatas[itemId][size] = 1;
+              }
+            }
+            else{
+                cartDatas[itemId] = {};
+                cartDatas[itemId][size] = 1;
+            }
+            setCartItems(cartDatas);
+        }
+
+        useEffect(() =>{
+          console.log(cartDatas);
+        },[cartItems])
        
        const value ={
-        products , currency , delvery_fee , search , setSearch , showSearch , setShowSearch
+        products , currency , delvery_fee , 
+        search , setSearch , showSearch , 
+        setShowSearch , cartItems , addToCart
      }
      return(
         <ShopContext.Provider value={value}>
