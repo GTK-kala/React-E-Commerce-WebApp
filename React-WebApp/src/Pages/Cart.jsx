e23f814f5917176bf200fb9664bb2c7650a7d3cc
GTK-kala <gtkkala1110@gmail.com>
2025-06-27 18:35:18 +0300

    Twenty-Three commit

--- React-WebApp/src/Pages/Cart.jsx
+++ React-WebApp/src/Pages/Cart.jsx
@@ -22,11 +22,12 @@ const Cart = () => {
        }
      }
    setCartData(tempData);
-  },[cartItems])
+  },[cartItems]);
+  
   const Array = ['YOUR','CART']
   return (
     <div className="border-t pt-14">
-      <div className="text-2xl mb-3">
+      <div className="mb-3 text-2xl">
         <Titile Text={Array}/>
       </div>
       <div >
@@ -38,15 +39,15 @@ const Cart = () => {
              <div  className="flex items-start gap-6">
                <img className="w-16 sm:w-20" src={productData.image[0]} alt="productData-Image" />
                <div>
-                <p className="text-xs sm:text-lg font-medium">{productData.name}</p>
+                <p className="text-xs font-medium sm:text-lg">{productData.name}</p>
                 <div className="flex items-center gap-5 mt-2">
                  <p>{currency} {productData.price}</p>
-                 <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">{item.size}</p>
+                 <p className="px-2 border sm:px-3 sm:py-1 bg-slate-50">{item.size}</p>
                 </div>
                </div>
              </div>
-             <input  className="border max-w-10 sm:max-w-20 px-1 sm:px-1 py-1" type="number" min={1} defaultValue={item.quantity}/>
-             <img onClick={() =>upDateQuantity(item._id,item.size,0)} className="w-4 m-4 sm:w-5 cursor-pointer" src={assets.bin_icon} alt="Bin-Icon" />
+             <input onChange={(e) =>{e.target.value === '' || e.target.value === '0' ? null : upDateQuantity(item._id,item.size,Number(e.target.value))}}  className="px-1 py-1 border max-w-10 sm:max-w-20 sm:px-1" type="number" min={0} defaultValue={item.quantity}/>
+             <img onClick={() =>upDateQuantity(item._id,item.size,0)} className="w-4 m-4 cursor-pointer sm:w-5" src={assets.bin_icon} alt="Bin-Icon" />
             </div>
            )
           })
