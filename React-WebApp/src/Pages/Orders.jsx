47ad46b95bb256f931ead4945ed787aa4fec4b4b
GTK-kala <gtkkala1110@gmail.com>
2025-07-02 15:37:31 +0300

    Twenty-Nine commit

--- React-WebApp/src/Pages/Orders.jsx
+++ React-WebApp/src/Pages/Orders.jsx
@@ -1,9 +1,50 @@
+import Title from "../Components/Title";
+import { ShopContext } from "../Context/ShopContext";
+import { useContext } from 'react';
+
 
 const Orders = () => {
+  
+  const {currency , products} = useContext(ShopContext);
+  let date = new Date();
+  date = date.toLocaleDateString();
+  const Array = ['MY','ORDERS'];
   return (
-    <>
-    
-    </>
+    <div className="border-t pt-16">
+
+     <div className="text-2xl">
+      <Title Text={Array} />
+     </div>
+
+     <div>
+      {
+        products.slice(1,4).map((item,i) =>{
+          return <div key={i} className="py-4 border-b text-gray-700 flex flex-col md:flex-row md:items-center justify-between gap-4">
+            <div className="flex items-start gap-6 text-sm">
+              <img className="w-16 sm:w-20 object-cover" src={item.image[0]} alt="" />
+              <div>
+                <p className="text-base font-medium">{item.name}</p>
+                <div className="flex items-center gap-3 mt-2 text-base text-gray-600">
+                <p className="text-lg">{currency}{item.price}</p>
+                <p>Quantity : 1</p>
+                <p>Size : M</p>
+                </div>
+                <p className="mt-2">Date : <span className="text-gray-400 pl-1">{date}</span></p>
+              </div>
+            </div>
+
+            <div className="flex justify-between md:w-1/2">
+              <div className="flex items-center gap-2">
+               <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
+               <p className="text-sm md:text-base">Ready to ship</p>
+              </div>
+              <button className="text-sm md:text-base border border-gray-300 rounded-md px-4 py-2">Track Order</button>
+            </div>
+          </div>
+        })
+      }
+     </div>
+    </div>
   )
 }
 
--- React-WebApp/src/Pages/PlaceOrder.jsx
+++ React-WebApp/src/Pages/PlaceOrder.jsx
@@ -1,9 +1,18 @@
 import Title from '../Components/Title';
+import CartTotal from '../Components/CartTotal';
+import { assets } from '../frontend_assets/assets';
+import  {useContext, useState} from 'react';
+import { ShopContext } from '../Context/ShopContext';
 
 
 const PlaceOrder = () => {
 
-  const Array = ['DELIVERY','INFORMATION']
+  const [method, setMethod] = useState('cod');
+  const {navigate} = useContext(ShopContext);
+
+  const Array = ['DELIVERY','INFORMATION'];
+  const Array2 = ['PAYMENT','METHOD'];
+
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14  min-h-[80vh] border-t">
 
@@ -31,7 +40,32 @@ const PlaceOrder = () => {
       </div>
 
     {/*------------------------Right Side---------------------- */}  
-     
+      <div className='mt-8'>
+       <div className='mt-8 min-w-80'>
+        <CartTotal />
+       </div>
+       <div className='mt-12'>
+        <Title Text={Array2} />
+    {/*------------------------Payment Method Section---------------------- */} 
+        <div className='flex flex-col gap-3 lg:flex-row'>
+          <div onClick={() => setMethod('stripe')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+            <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-500' : ''}`}></p>
+            <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
+          </div>
+          <div onClick={() => setMethod('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+            <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-green-500' : ''}`}></p>
+            <img className='h-5 mx-4' src={assets.razorpay_logo} alt="" />
+          </div>
+          <div onClick={() => setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+            <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-500' : ''}`}></p>
+            <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
+          </div>
+        </div>
+         <div className='w-full text-end mt-8'>
+           <button onClick={() => navigate('/orders')} className='bg-blue-500 text-white py-2 px-4 rounded'>Place Order</button>
+         </div>
+       </div>
+      </div>
     </div>
   )
 }
