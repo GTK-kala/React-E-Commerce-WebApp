2544417e2086387aceb6b1668881a121bfdc0ab1
GTK-kala <gtkkala1110@gmail.com>
2025-07-02 13:12:11 +0300

    Twenty-Eight commit

--- React-WebApp/src/Pages/PlaceOrder.jsx
+++ React-WebApp/src/Pages/PlaceOrder.jsx
@@ -1,10 +1,38 @@
+import Title from '../Components/Title';
 
 
 const PlaceOrder = () => {
+
+  const Array = ['DELIVERY','INFORMATION']
   return (
-    <>
-    
-    </>
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14  min-h-[80vh] border-t">
+
+    {/*------------------------Left Side---------------------- */}
+
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        <div className="text-1xl sm:text-2xl my-3">
+          <Title Text={Array}/>
+        </div>
+        <div className="flex gap-3">
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='First name...'/>
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Last name...'/>
+        </div>
+        <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="email" placeholder='Email address...'/>
+        <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Street...'/>
+        <div className="flex gap-3">
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='City...'/>
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='State...'/>
+        </div>
+        <div className="flex gap-3">
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="number" placeholder='zip-code...'/>
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Country...'/>
+        </div>
+        <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="number" placeholder='Phone-no...'/>
+      </div>
+
+    {/*------------------------Right Side---------------------- */}  
+     
+    </div>
   )
 }
 
